51b05edbfe368a12c15b62e2fbdd9135994e1878
Richard Kiene <richard.kiene@gmail.com>
2020-07-22 16:31:34 -0700

    Use async run function wrapper

--- index.js
+++ index.js
@@ -2,43 +2,47 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const semver = require('semver');
 
-try {
-    const githubToken = core.getInput('github-token');
-    const packagePath = core.getInput('package-file');
-    const octokit = new github.getOctokit(githubToken);
+async function run() {
+    try {
+        const githubToken = core.getInput('github-token');
+        const packagePath = core.getInput('package-file');
+        const octokit = new github.getOctokit(githubToken);
 
-    /* Fetch current package.json */
-    const curRef = github.context.sha;
-    const curPackageJson = (await octokit.repos.getContent({
-        ...github.context.repo,
-        path: packagePath,
-        curRef
-    })).data.content;
-    const curVersion = JSON.parse(Buffer.from(curPackageJson, 'base64').toString()).version;
+        /* Fetch current package.json */
+        const curRef = github.context.sha;
+        const curPackageJson = (await octokit.repos.getContent({
+            ...github.context.repo,
+            path: packagePath,
+            curRef
+        })).data.content;
+        const curVersion = Number(JSON.parse(Buffer.from(curPackageJson, 'base64').toString()).version);
 
-    /* Fetch previous package.json */
-    const prevRef = ((await octokit.repos.getCommit({
-        ...github.context.repo,
-        ref: curRef
-    })).data.parents[0] || {}).sha;
-    const prevPackageJson = (await octokit.repos.getContent({
-        ...github.context.repo,
-        path: packagePath,
-        prevRef
-    })).data.content;
-    const prevVersion = JSON.parse(Buffer.from(prevPackageJson, 'base64').toString()).version;
+        /* Fetch previous package.json */
+        const prevRef = ((await octokit.repos.getCommit({
+            ...github.context.repo,
+            ref: curRef
+        })).data.parents[0] || {}).sha;
+        const prevPackageJson = (await octokit.repos.getContent({
+            ...github.context.repo,
+            path: packagePath,
+            prevRef
+        })).data.content;
+        const prevVersion = Number(JSON.parse(Buffer.from(prevPackageJson, 'base64').toString()).version);
 
-    /* Check that the current pull request package.json version property has been incremented */
-    if (semver.valid(curVersion) && semver.gt(curVersion, prevVersion)) {
-        core.setOutput("is_incremented", true);
-    } else {
-        const errorMessage = 'package.json version value was not incremented';
-        core.setOutput("is_incremented", false);
-        core.error(errorMessage);
-        core.setFailed(errorMessage);
+        /* Check that the current pull request package.json version property has been incremented */
+        if (curVersion > prevVersion) {
+            core.setOutput("is_incremented", true);
+        } else {
+            const errorMessage = 'package.json version value was not incremented';
+            core.setOutput("is_incremented", false);
+            core.error(errorMessage);
+            core.setFailed(errorMessage);
+        }
+
+    } catch (error) {
+        core.error(error);
+        core.setFailed(error.message);
     }
-
-} catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
 }
+
+run().then();
